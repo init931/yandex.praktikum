fe81d87d3e768a96a3c50d8973b47fc551906d4a
init931 <friendskenny72@gmail.com>
2021-05-03 14:52:47 +0600

    add fast_test.cpp

---
+++ fast_test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <set>
+#include <map>
+#include <cassert>
+#include <sstream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    std::cout << "hi"s << std::endl;
+    return 0;
+}
